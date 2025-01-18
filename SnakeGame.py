ccc386f622bac6f224ae9bc2954b125354c68191
MOKOJOPE <momoinbb21@gmail.com>
2025-01-17 22:22:39 -1000

    Orgainedwith functions and add action to make a food

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,74 +1,104 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
 #Dictionary
+
+Colors = {
+    "MintyGreen": (40,210,180),
+    "Red": (255,0,0),
+    "LightBlue":(0,120,255)
+}
+
 Config = {
     "ScreenX":800,
     "ScreenY":600,
     "ScreenTittle": "Momo's Snake Game",
-    "MintyGreen": (40,210,180),
-    "Red": (255,0,0),
+    "Background": Colors["MintyGreen"],
     "BlockSize": 10, 
-    "Speed": 20
+    "Speed": 15
 }
 
 Snake = {
     "X": Config["ScreenX"] / 2,
     "Y": Config["ScreenY"] / 2, 
-    "Direction": "none"
+    "Direction": "none",
+    "Color": Colors["Red"]
 }
 
-#Backgroup Setting
-screen = pygame.display.set_mode((Config["ScreenX"], Config["ScreenY"]))
-clock = pygame.time.Clock()
-pygame.display.set_caption(Config["ScreenTittle"])
-pygame.display.update()
+Food = {
+    "X": 0,
+    "Y": 0,
+    "Color": Colors["LightBlue"]
+}
 
+def RandmoizeFoodlocation():
+    Food["X"] = round(random.randrange(0,Config["ScreenX"]-Config["BlockSize"]),-1)
+    Food["Y"] = round(random.randrange(0,Config["ScreenY"]-Config["BlockSize"]),-1)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        #More than one case
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                Snake["Direction"] = "left"
-                
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                Snake["Direction"] = "right"
-                
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
-                Snake["Direction"] = "up"
-                
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                Snake["Direction"] = "down"
-                
-    #If we don't set a clock for speed, it will move super fast like computer XD
-    if Snake["Direction"] == "left":
-        Snake["X"] -= Config["BlockSize"]
-    if Snake["Direction"] == "right":
-        Snake["X"] += Config["BlockSize"]
-    if Snake["Direction"] == "up":
-        Snake["Y"] -= Config["BlockSize"]
-    if Snake["Direction"] == "down":
-        Snake["Y"] += Config["BlockSize"]
-    
-                    
-            
-
+def DrawGame(screen):
     #Frames stacking on top of each other
-    screen.fill(Config["MintyGreen"])
-    pygame.draw.rect(screen, Config["Red"], [Snake["X"], Snake["Y"], Config["BlockSize"], Config["BlockSize"]])
+    screen.fill(Config["Background"])
+    pygame.draw.rect(screen, Snake["Color"], [Snake["X"], Snake["Y"], Config["BlockSize"], Config["BlockSize"]])
+    pygame.draw.rect(screen, Food["Color"], [Food["X"], Food["Y"], Config["BlockSize"], Config["BlockSize"]])
     pygame.display.update()
 
-    #Check for wall collision
-    if Snake["X"] < 0 or Snake["X"] >= Config["ScreenX"] or Snake["Y"] < 0 or Snake["Y"] >= Config["ScreenY"]:
-        break
-    
-    #Control the refesh rate
-    clock.tick(Config["Speed"])
+def main():
+    #Backgroup Setting
+    screen = pygame.display.set_mode((Config["ScreenX"], Config["ScreenY"]))
+    clock = pygame.time.Clock()
+    pygame.display.set_caption(Config["ScreenTittle"])
+    pygame.display.update()
+    RandmoizeFoodlocation()
 
-print("You hit the wall!!") 
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            #More than one case
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    Snake["Direction"] = "left"
+                    
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    Snake["Direction"] = "right"
+                    
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    Snake["Direction"] = "up"
+                    
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    Snake["Direction"] = "down"
+                    
+        #If we don't set a clock for speed, it will move super fast like computer XD
+        if Snake["Direction"] == "left":
+            Snake["X"] -= Config["BlockSize"]
+        if Snake["Direction"] == "right":
+            Snake["X"] += Config["BlockSize"]
+        if Snake["Direction"] == "up":
+            Snake["Y"] -= Config["BlockSize"]
+        if Snake["Direction"] == "down":
+            Snake["Y"] += Config["BlockSize"]
+        
+
+        DrawGame(screen)
+        
+        #Check for wall collision
+        if Snake["X"] < 0 or Snake["X"] >= Config["ScreenX"] or Snake["Y"] < 0 or Snake["Y"] >= Config["ScreenY"]:
+            break
+
+        if Snake["X"] == Food["X"] and Snake["Y"] == Food["Y"]:
+            print("You est a yummy apple!")
+            RandmoizeFoodlocation()
+        
+        #Control the refesh rate
+        clock.tick(Config["Speed"])
+
+    print("You hit the wall!!")
+
+if __name__ == "__main__":
+    main()
+
 
